afa74036dca0c47a7d27fdb2b6b280fb1f35dda8
Subashkannan <subashkannan0309@gmail.com>
2025-11-29 19:37:58 +0530

    update features

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import Poster from "../Components/Poster";
 import FeaturedProducts from "../Components/Home/FeaturedProducts";
-import CategoriesGrid from "../Components/Home/CategoriesGrid";
 import Features from "../Components/Home/Features";
-import Newsletter from "../Components/Home/Newsletter";
 
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Poster />
-      <CategoriesGrid />
       <FeaturedProducts />
       <Features />
-      <Newsletter />
     </div>
   );
 };
